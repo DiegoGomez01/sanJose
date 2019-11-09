eac89cd0bb696d26eba31067b9f4e32488f19c89
Diego F. Gómez <gomezdiego1998@gmail.com>
2019-11-08 19:44:14 -0500

    add view component foro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ForoComponent } from './foro/foro.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     WelcomeComponent,
     HomeComponent,
     PageNotFoundComponent,
-    NavbarComponent
+    NavbarComponent,
+    ForoComponent
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,16 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  openConversemos = false;
+  openBarrio = false;
+  openPropondremos = false;
+  openNosotros = false;
+  openSanJose = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  test() {
+  showConversemos() {
+    this.openConversemos = true;
     document.getElementById('divBackground').style.backgroundImage = 'url("/assets/images/conversemos.jpg")';
   }
 
+  showLoMejorDeMiBarrio() {
+    this.openBarrio = true;
+    document.getElementById('divBackground').style.backgroundImage = 'url("/assets/images/loMejorDeMiBarrio.jpg")';
+  }
+
+  showQueProponemos() {
+    this.openPropondremos = true;
+    document.getElementById('divBackground').style.backgroundImage = 'url("/assets/images/queProponemos.jpg")';
+  }
+
+  showQuienesSomos() {
+    this.openNosotros = true;
+    document.getElementById('divBackground').style.backgroundImage = 'url("/assets/images/quienesSomos.jpg")';
+  }
+
+  showSanJose() {
+    this.openSanJose = true;
+    document.getElementById('divBackground').style.backgroundImage = 'url("/assets/images/sanJoseInteligente.jpg")';
+  }
+
   defaultBackground() {
+    this.openNosotros = false;
+    this.openBarrio = false;
+    this.openConversemos = false;
+    this.openSanJose = false;
+    this.openPropondremos = false;
     document.getElementById('divBackground').style.backgroundImage = 'url("/assets/images/general.jpg")';
   }
 
